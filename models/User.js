de9a717019440040437beec1a8ff42baadd0b8e3
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-09-04 13:49:46 -0700

    User schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,20 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-
+    username: { 
+        type: String, 
+        required: true,
+        unique: true,
+        set: v => v.trim()
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/u
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
 
 const User = model('User', userSchema);

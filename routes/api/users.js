a0d7ab78052e2074f79a8cb6857d2f646ef53f56
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-09-04 14:31:06 -0700

    GET api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,27 @@
 const router = require('express').Router();
 const User = require('../../models/User');
 
+// Create a user
 router.post('/', async (req, res) => {
     try {
         const user = await User.create({
             username: req.body.username,
             email: req.body.email
         });
-        res.json(user);
+        res.status(200).json(user);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;

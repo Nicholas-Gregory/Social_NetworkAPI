ea434e17423def7224284af8c93c1301606d73f2
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-09-04 17:59:49 -0700

    POST /api/thoughts

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
+const Thought = require('../../models/Thought');
+const User = require('../../models/User');
+
+router.post('/', async (req, res) => {
+    const body = req.body;
+
+    try {
+        const user = await User.findOne({ username: body.username });
+
+        if (!user) {
+            return res.status(404).send("No user with that username exists");
+        }
+
+        const thought = await Thought.create({
+            thoughtText: body.thoughtText,
+            createdAt: body.createdAt,
+            username: body.username
+        });
+        
+        user.thoughts.push(thought.id);
+        await user.save();
+
+        res.status(200).json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
